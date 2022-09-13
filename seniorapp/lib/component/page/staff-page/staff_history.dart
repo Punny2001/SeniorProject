482d164185a3daaf584a580ad72ea3d59dd6f41d
Krissanapong Palakham <krissanapong.pal@student.mahidol.ac.th>
2022-09-13 23:13:41 +0700

    Updated 13/9/2022
    Updated Coding
    - Staff history: multiple stream

--- seniorapp/lib/component/page/staff-page/staff_history.dart
+++ seniorapp/lib/component/page/staff-page/staff_history.dart
@@ -10,6 +10,8 @@ import 'package:seniorapp/component/page/staff-page/staff_report_page/injury_rep
 import 'package:seniorapp/component/report-data/illness_report_data.dart';
 import 'package:seniorapp/component/report-data/injury_report_data.dart';
 import 'package:rxdart/rxdart.dart';
+import 'dart:async' show Stream, StreamController;
+import 'package:async/async.dart' show StreamZip;
 
 class StaffReport extends StatefulWidget {
   const StaffReport({Key key}) : super(key: key);
@@ -22,16 +24,10 @@ class _StaffReportState extends State<StaffReport> {
   @override
   Widget build(BuildContext context) {
     String uid = FirebaseAuth.instance.currentUser.uid;
-    Stream illnessStream = FirebaseFirestore.instance
-        .collection('IllnessReport')
-        .where('staff_uid', isEqualTo: uid)
-        .snapshots();
-    Stream injuryStream = FirebaseFirestore.instance
-        .collection('InjuryReport')
-        .where('staff_uid', isEqualTo: uid)
-        .snapshots();
+    StreamController<QuerySnapshot> controller =
+        StreamController<QuerySnapshot>.broadcast();
 
-    Stream<QuerySnapshot<Object>> getData() {
+    Stream<List<QuerySnapshot>> getData() {
       Stream illnessStream = FirebaseFirestore.instance
           .collection('IllnessReport')
           .where('staff_uid', isEqualTo: uid)
@@ -40,49 +36,42 @@ class _StaffReportState extends State<StaffReport> {
           .collection('InjuryReport')
           .where('staff_uid', isEqualTo: uid)
           .snapshots();
+      print(StreamZip([injuryStream, illnessStream]));
 
-      Stream combined = CombineLatestStream.list([illnessStream, injuryStream]);
-      return combined;
+      return StreamZip([injuryStream, illnessStream]);
     }
 
     return Scaffold(
       body: Container(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: getData(),
-          builder: (BuildContext context, snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemCount: snapshot.data.docs.length,
+        child: StreamBuilder(
+            stream: getData(),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.hasData) {
+                List<QuerySnapshot> querySnapshot = snapshot.data.toList();
+                List<String> docID = snapshot.data.docs.id;
+                List<QueryDocumentSnapshot> documentSnapshot = [];
+
+                querySnapshot.forEach((query) {
+                  documentSnapshot.addAll(query.docs);
+                });
+
+                List<Map<String, dynamic>> mappedData = [];
+                for (QueryDocumentSnapshot doc in documentSnapshot) {
+                    mappedData.add(doc.data());
+                }
+
+                return ListView.builder(
+                    itemCount: mappedData.length,
                     itemBuilder: (context, index) {
-                      DocumentSnapshot data = snapshot.data.docs[index];
+                      Map<String, dynamic> data = mappedData[index];
                       return GestureDetector(
-                        child: data['report_type'] == 'Illness'
-                            ? Card(
+                        child: Card(
                                 margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
                                 child: Column(
                                   children: <Widget>[
                                     Column(
                                       children: <Widget>[
-                                        Text(data.id),
-                                        Text(data['report_type']),
-                                        Text(data['sport_event']),
-                                        // data['report_type'] == 'Illness'
-                                        //     ? Text(formatDate(
-                                        //         data["occured_date"].toDate()))
-                                        //     : Text(formatDate(
-                                        //         data["injuryDateTime"].toDate()))
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              )
-                            : Card(
-                                margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                                child: Column(
-                                  children: <Widget>[
-                                    Column(
-                                      children: <Widget>[
-                                        Text(data.id),
+                                        Text(data.)
                                         Text(data['report_type']),
                                         Text(data['sport_event']),
                                         // data['report_type'] == 'Illness'
@@ -95,43 +84,95 @@ class _StaffReportState extends State<StaffReport> {
                                   ],
                                 ),
                               ),
-                        onTap: () {
-                          IllnessReportData illness =
-                              IllnessReportData.fromMap(data.data());
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => ReportIllnessDescription(
-                                report_id: data.id,
-                                affected_system: illness.affected_system,
-                                affected_system_code:
-                                    illness.affected_system_code,
-                                athlete_no: illness.athlete_no,
-                                diagnosis: illness.diagnosis,
-                                illness_cause: illness.illness_cause,
-                                illness_cause_code: illness.illness_cause_code,
-                                mainSymptoms: illness.mainSymptoms,
-                                mainSymptomsCode: illness.mainSymptomsCode,
-                                no_day: illness.no_day,
-                                occured_date: illness.occured_date,
-                                report_type: illness.report_type,
-                                sport_event: illness.sport_event,
-                                staff_uid: illness.staff_uid,
-                              ),
-                            ),
-                          );
-                        },
                       );
-                    },
-                  )
-                : const Center(
-                    child: CircularProgressIndicator(),
-                  );
-          },
-        ),
+                    });
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }),
       ),
     );
   }
 }
+
+// ListView.builder(
+//                     itemCount: snapshot.data.docs.length,
+//                     itemBuilder: (context, index) {
+//                       DocumentSnapshot data = snapshot.data.docs[index];
+//                       return GestureDetector(
+//                         child: data['report_type'] == 'Illness'
+//                             ? Card(
+//                                 margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+//                                 child: Column(
+//                                   children: <Widget>[
+//                                     Column(
+//                                       children: <Widget>[
+//                                         Text(data.id),
+//                                         Text(data['report_type']),
+//                                         Text(data['sport_event']),
+//                                         // data['report_type'] == 'Illness'
+//                                         //     ? Text(formatDate(
+//                                         //         data["occured_date"].toDate()))
+//                                         //     : Text(formatDate(
+//                                         //         data["injuryDateTime"].toDate()))
+//                                       ],
+//                                     ),
+//                                   ],
+//                                 ),
+//                               )
+//                             : Card(
+//                                 margin: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+//                                 child: Column(
+//                                   children: <Widget>[
+//                                     Column(
+//                                       children: <Widget>[
+//                                         Text(data.id),
+//                                         Text(data['report_type']),
+//                                         Text(data['sport_event']),
+//                                         // data['report_type'] == 'Illness'
+//                                         //     ? Text(formatDate(
+//                                         //         data["occured_date"].toDate()))
+//                                         //     : Text(formatDate(
+//                                         //         data["injuryDateTime"].toDate()))
+//                                       ],
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                         onTap: () {
+//                           IllnessReportData illness =
+//                               IllnessReportData.fromMap(data.data());
+//                           Navigator.of(context).push(
+//                             MaterialPageRoute(
+//                               builder: (context) => ReportIllnessDescription(
+//                                 report_id: data.id,
+//                                 affected_system: illness.affected_system,
+//                                 affected_system_code:
+//                                     illness.affected_system_code,
+//                                 athlete_no: illness.athlete_no,
+//                                 diagnosis: illness.diagnosis,
+//                                 illness_cause: illness.illness_cause,
+//                                 illness_cause_code: illness.illness_cause_code,
+//                                 mainSymptoms: illness.mainSymptoms,
+//                                 mainSymptomsCode: illness.mainSymptomsCode,
+//                                 no_day: illness.no_day,
+//                                 occured_date: illness.occured_date,
+//                                 report_type: illness.report_type,
+//                                 sport_event: illness.sport_event,
+//                                 staff_uid: illness.staff_uid,
+//                               ),
+//                             ),
+//                           );
+//                         },
+//                       );
+//                     },
+//                   )
+//                 : const Center(
+//                     child: CircularProgressIndicator(),
+//                   );
+//           },
 
 // child: Column(
 //           children: [
