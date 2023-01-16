bac26bc04c1fa9f1346e94d997ef47a212cdd196
Punny <punza15@hotmail.com>
2023-01-17 01:56:16 +0700

    Update AppDelegate.swift

--- seniorapp/ios/Runner/AppDelegate.swift
+++ seniorapp/ios/Runner/AppDelegate.swift
@@ -9,15 +9,9 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      FirebaseApp.configure()
+    GMSServices.provideAPIKey("AAAAOmXVBT0:APA91bFonAMAsnJl3UDp2LQHXvThSOQd2j7q01EL1afdZI13TP7VEZxRa7q_Odj3wUL_urjyfS7e0wbgEbwKbUKPkm8p5LFLAVE498z3X4VgNaR5iMF4M9JMpv8s14YsGqI2plf_lCBK")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
-        Messaging.messaging().apnsToken = deviceToken
-        print("Token: \(deviceToken)")
-        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
 }
 
